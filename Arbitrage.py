77b17fbcf245bc040d2f4e68573d943940d7734a
r12922150 <r12922150@ntu.edu.tw>
2024-04-12 16:03:20 +0800

    update python script and readme

--- Arbitrage.py
+++ Arbitrage.py
@@ -10,3 +10,95 @@ liquidity = {
     ("tokenC", "tokenE"): (10, 8),
     ("tokenD", "tokenE"): (60, 25),
 }
+
+
+
+from typing import Tuple, List
+from math import log
+
+def negate_logarithm_convertor(graph: Tuple[Tuple[float]]) -> List[List[float]]:
+    ''' log of each rate in graph and negate it'''
+    result = [[-log(edge) for edge in row] for row in graph]
+    return result
+
+
+def arbitrage(currency_tuple: tuple, rates_matrix: Tuple[Tuple[float, ...]]):
+    ''' Calculates arbitrage situations and prints out the details of this calculations'''
+
+    trans_graph = negate_logarithm_convertor(rates_matrix)
+
+    # Pick any source vertex -- we can run Bellman-Ford from any vertex and get the right result
+
+    source = 1
+    n = len(trans_graph)
+    min_dist = [float('inf')] * n
+
+    pre = [-1] * n
+    
+    min_dist[source] = 0
+
+    # 'Relax edges |V-1| times'
+    for _ in range(n-1):
+        for source_curr in range(n):
+            for dest_curr in range(n):
+                if min_dist[dest_curr] > min_dist[source_curr] + trans_graph[source_curr][dest_curr]:
+                    min_dist[dest_curr] = min_dist[source_curr] + trans_graph[source_curr][dest_curr]
+                    pre[dest_curr] = source_curr
+
+    # if we can still relax edges, then we have a negative cycle
+    for source_curr in range(n):
+        for dest_curr in range(n):
+            if min_dist[dest_curr] > min_dist[source_curr] + trans_graph[source_curr][dest_curr]:
+                # negative cycle exists, and use the predecessor chain to print the cycle
+                print_cycle = [dest_curr, source_curr]
+                # Start from the source and go backwards until you see the source vertex again or any vertex that already exists in print_cycle array
+                while pre[source_curr] not in  print_cycle:
+                    print_cycle.append(pre[source_curr])
+                    source_curr = pre[source_curr]
+                print_cycle.append(pre[source_curr])
+                # print("Arbitrage Opportunity: \n")
+                # print(" --> ".join([currencies[p] for p in print_cycle[::-1]]))
+                possible_paths.append([currencies[p] for p in print_cycle[::-1]])
+
+def swap_for_exact_token(token_in, token_out, amount_in):
+    if (token_in, token_out) in liquidity:
+        y, x= liquidity[(token_in, token_out)]
+    else:
+        x, y = liquidity[(token_out, token_in)]
+    return (0.997*x*amount_in)/(y + 0.997*amount_in)
+
+currencies = ('tokenA', 'tokenB', 'tokenC', 'tokenD', 'tokenE')
+
+rates = [[1]*len(currencies) for i in range(len(currencies))]
+
+for i in range(len(currencies)):
+    for j in range(i+1, len(currencies)):
+        x, y = liquidity[(currencies[i], currencies[j])]
+        rates[i][j] = y/x
+        rates[j][i] = x/y
+        
+possible_paths = []
+arbitrage(currencies, rates)
+
+for path in possible_paths:
+    curr_token = 'tokenB'
+    curr_value = 5
+
+    if path[0] == 'tokenA':
+        path = ['tokenB'] + path
+    elif path[0] == 'tokenC' or path[0] == 'tokenD' or path[0] == 'tokenE':
+        path = ['tokenB', 'tokenA'] + path
+
+    extended_path = ['tokenB']
+
+    for i in range(1, len(path)):
+        extended_path.append(path[i])
+        if path[i] == curr_token:
+            break        
+        
+    for i in range(len(extended_path)-1):
+        curr_value = swap_for_exact_token(extended_path[i], extended_path[i+1], curr_value)
+        curr_token = extended_path[i+1]
+    if curr_value > 20:
+        print('path:', '->'.join(extended_path), end=', ')
+        print(f'{curr_token} balance={curr_value}')
--- README.md
+++ README.md
@@ -7,23 +7,57 @@ Provide your profitable path, the amountIn, amountOut value for each swap, and y
 
 > Solution
 
+(tokenB -> tokenA) : (5, 5.655321988655323)
+
+(tokenA -> tokenC) : (5.655321988655323, 2.372138936383089)
+
+(tokenC -> tokenE) : (2.372138936383089, 1.530137136963617)
+
+(tokenE -> tokenD) : (1.530137136963617, 3.4507414486197083)
+
+(tokenD -> tokenC) : (3.4507414486197083, 6.684525579572587)
+
+(tokenC -> tokenB) : (6.684525579572587, 22.497221806974142)
+
+path: tokenB->tokenA->tokenC->tokenE->tokenD->tokenC->tokenB, tokenB balance=22.497221806974142
+
+
 ## Problem 2
 What is slippage in AMM, and how does Uniswap V2 address this issue? Please illustrate with a function as an example.
 
 > Solution
+
+Slippage in an Automated Market Maker (AMM) context refers to the difference between the expected price of a token swap and the actual price achieved when the trade is executed. This difference arises because AMM liquidity pools rely on supply and demand to determine token prices.
+
+uniswapV2 revert transection when expected amountOut < amountOutMin
+```
+amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path);
+require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
+```
+
 
 ## Problem 3
 Please examine the mint function in the UniswapV2Pair contract. Upon initial liquidity minting, a minimum liquidity is subtracted. What is the rationale behind this design?
 
 > Solution
 
+Some LP token is burned to ensure no one has all the LP tokens, preventing "inflation attack".
+
 ## Problem 4
 Investigate the minting function in the UniswapV2Pair contract. When depositing tokens (not for the first time), liquidity can only be obtained using a specific formula. What is the intention behind this?
 
 > Solution
 
+This formula provide incentivizes provider to increase the supply of token0 and token1 without changing the ratio of token0 and token1.
+
+And prevent someone from getting much more value of liquidity than he deposit.
+
 ## Problem 5
 What is a sandwich attack, and how might it impact you when initiating a swap?
 
 > Solution
+
+Sandwich attack is someone front-run my transection, reduce the expact value to my lowest slippage range.
+
+After my transection done, they swap their transection to gain interest.
 
